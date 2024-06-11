171b05bf1ffe05ab68db579ebc7369bdd953f1f4
keshavsoft <nknnkumar@yahoo.com>
2024-06-11 23:22:28 +0530

    keshav

--- resources/js/FromTally/EntryFile.js
+++ resources/js/FromTally/EntryFile.js
@@ -1,8 +1,13 @@
 let StartFunc = async () => {
-
     let jVarLocalXml = await jFLocalGetXml();
-    console.log("jVarLocalXml : ", jVarLocalXml);
-    await FromTally({ inXml: jVarLocalXml });
+
+    let jVarLocalItemData = await FromTally({ inXml: jVarLocalXml });
+    let jVarLocalItemsJson = jFLocalXmlToJson({ inXmlFromTally: jVarLocalItemData });
+    console.log("jVarLocalItemsJson : ", jVarLocalItemsJson);
+    var $table = $('#table');
+    $table.bootstrapTable({
+        data: jVarLocalItemsJson
+    });
 };
 
 let FromTally = async ({ inXml }) => {
@@ -18,7 +23,7 @@ let FromTally = async ({ inXml }) => {
     let r2 = await fetch("http://localhost:9000/", config);
     let d2 = await r2.text();
     // console.log("jVarLocalData /; ", d2);
-    document.getElementById("HeadId").innerHTML = d2;
+    return d2;
 };
 
 let jFLocalGetXml = async () => {
@@ -28,6 +33,30 @@ let jFLocalGetXml = async () => {
     let jVarLocalData = await jVarLocalResponse.text();
     return jVarLocalData;
 
+};
+
+const jFLocalXmlToJson = ({ inXmlFromTally }) => {
+    let jVarLocalItemsXml = inXmlFromTally;
+    let ReturnArray = [];
+
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(jVarLocalItemsXml, "application/xml");
+
+    let checkboxes = doc.documentElement.querySelectorAll("STOCKITEMS");
+
+    checkboxes.forEach((userItem) => {
+        let LoopInsideObject = {};
+
+        let LoopInsideRate = userItem.querySelector("KSSTOCKITEMNAME");
+
+        if (LoopInsideRate === null === false) {
+            LoopInsideObject.ItemName = LoopInsideRate.innerHTML;
+        };
+
+        ReturnArray.push(LoopInsideObject);
+    });
+
+    return ReturnArray;
 };
 
 StartFunc().then();
