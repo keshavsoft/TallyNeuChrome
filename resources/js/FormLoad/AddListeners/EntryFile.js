6b4c78e4e4e77a5ed14a77319973b6a90d650e72
Keshav <nknnkumar@yahoo.com>
2024-06-13 14:53:24 +0530

    keshav

--- resources/js/FormLoad/AddListeners/EntryFile.js
+++ resources/js/FormLoad/AddListeners/EntryFile.js
@@ -1,9 +1,11 @@
 import { StartFunc as StartFuncItemsId } from "./ItemsId/EntryFile.js";
 import { StartFunc as StartFuncAccountsId } from "./AccountsId/EntryFile.js";
+import { StartFunc as StartFuncStockCategoryId } from "./StockCategoryId/EntryFile.js";
 
 let StartFunc = () => {
     StartFuncItemsId();
     StartFuncAccountsId();
+    StartFuncStockCategoryId();
 };
 
 export { StartFunc };
